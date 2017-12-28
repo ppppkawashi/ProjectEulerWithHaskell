21ecb117cf20f6a1f6acfc19fdd8163fc9e9f649
kawashi <aisatu_nomahou@yahoo.co.jp>
2017-12-29 01:11:47 +0900

    いい感じにリファクタした

--- Problem001.hs
+++ Problem001.hs
@@ -1,8 +1,1 @@
-f (x:xs) = 
-    f xs + if mod x 3 == 0 || mod x 5 == 0 then x else 0
-f _ = 0
-
-main = do
-    print $ f a
-    where
-        a = [0..1000]
+main = print . sum $ filter (\x -> x `mod` 3 == 0 || x `mod` 5 == 0) [0..10000]
